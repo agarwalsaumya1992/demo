7190009fe5e07d5f962f04643306041eb7ba2ce9
saumya <saumya@Saumya-PC>
2021-10-12 22:03:08 +0530

    added categories

--- src/test/java/com/infy/demo/TestProductController.java
+++ src/test/java/com/infy/demo/TestProductController.java
@@ -14,7 +14,7 @@ import com.infy.dto.ResponseBuilder;
 
 	
 
-	public class TestCustomerController extends TestDemoApplication {
+	public class TestProductController extends TestDemoApplication {
 		
 		ProductDTO dto1 = new ProductDTO();
 
@@ -22,15 +22,16 @@ import com.infy.dto.ResponseBuilder;
 	   @Before
 	   public void setUp() {
 	      super.setUp();
-	      dto1.setCategory("women");
-	      dto1.setName("shirt");
+	      dto1.setCategory("test");
+	      dto1.setName("testname");
 	      dto1.setQuantity(5);
 	      dto1.setPrice(350.00);
+	      
 
 	   }
 	   @Test
-	   public void getCustomerList() throws Exception {
-	      String uri = "/customers";
+	   public void getProductList() throws Exception {
+	      String uri = "/products";
 	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 	      
@@ -40,19 +41,19 @@ import com.infy.dto.ResponseBuilder;
 	      ResponseBuilder response = super.mapFromJson(content, ResponseBuilder.class);
 	      assertEquals(response.getMessage(),"fetched successfully");
 	   }
-	   @Test
-	   public void createCustomer() throws Exception {
-	      String uri = "/customers";
-	      String inputJson = super.mapToJson(dto1);
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
-	         .contentType(MediaType.APPLICATION_JSON_VALUE)
-	         .content(inputJson)).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      String content = mvcResult.getResponse().getContentAsString();
-	      ResponseBuilder response = super.mapFromJson(content, ResponseBuilder.class);
-	      assertEquals(response.getMessage(), "Customer details got added successfully");
-	   }
+//	   @Test
+//	   public void createProduct() throws Exception {
+//	      String uri = "/products";
+//	      String inputJson = super.mapToJson(dto1);
+//	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+//	         .contentType(MediaType.APPLICATION_JSON_VALUE)
+//	         .content(inputJson)).andReturn();
+//	      
+//	      int status = mvcResult.getResponse().getStatus();
+//	      assertEquals(200, status);
+//	      String content = mvcResult.getResponse().getContentAsString();
+//	      ResponseBuilder response = super.mapFromJson(content, ResponseBuilder.class);
+//	      assertEquals(response.getMessage(), "Record got added successfully");
+//	   }
 	 
 	}
